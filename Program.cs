fefc1ee918b0f5375f84a5f8b2c10e1fd9271edb
agata olczak-peuker <pojker@gmail.com>
2024-02-17 20:21:50 +0100

    commit with var Friend

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+string aFriend = "Bill";
+Console.WriteLine("Hello " + aFriend);
+
+aFriend = "Maira";
+Console.WriteLine("Hello " + aFriend);
+
+Console.WriteLine($"Hello {aFriend}");
