15960686651acc0d6c400152cae226750b3fbd67
agata olczak-peuker <pojker@gmail.com>
2024-02-17 22:09:32 +0100

    string.StartsWith & string.EndsWith - True, False

--- Program.cs
+++ Program.cs
@@ -41,3 +41,9 @@ Console.WriteLine(sayHello.ToLower());
 string songLyrics = "You say goodbye, and I say hello";
 Console.WriteLine(songLyrics.Contains ("goodbye"));
 Console.WriteLine(songLyrics.Contains ("greetings"));
+
+
+Console.WriteLine(songLyrics.StartsWith ("You"));
+Console.WriteLine(songLyrics.StartsWith ("goodbye"));
+Console.WriteLine(songLyrics.EndsWith ("hello"));
+Console.WriteLine(songLyrics.EndsWith ("goodbye"));
