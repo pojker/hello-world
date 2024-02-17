980113413d95ec831c5f81bf60451eabe05e2a8e
agata olczak-peuker <pojker@gmail.com>
2024-02-17 21:04:14 +0100

    Srting.ToUpper and .ToLower cases

--- Program.cs
+++ Program.cs
@@ -32,3 +32,6 @@ Console.WriteLine(sayHello);
 
 sayHello = sayHello.Replace("Hello", "Greetings");
 Console.WriteLine(sayHello);
+
+Console.WriteLine(sayHello.ToUpper());
+Console.WriteLine(sayHello.ToLower());
