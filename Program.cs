971ef1be94040ccb386c6483b36940cfa310cfb5
agata olczak-peuker <pojker@gmail.com>
2024-02-17 20:34:53 +0100

    {string.Lenght} - show number of letter

--- Program.cs
+++ Program.cs
@@ -12,3 +12,6 @@ Console.WriteLine($"Hello {aFriend}");
 string firstFriend = "Maria";
 string secondFriend = "Sage";
 Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+
+Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters.");
+Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters.");
